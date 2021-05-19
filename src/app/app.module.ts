b3df3480031797692597ca16a937a5d8c37149d7
Satyakrishna Jasti <satyajasti9@gmail.com>
2021-05-19 20:06:08 +0530

    datetime completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,20 @@ import { DateComponent } from './date/date.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS } from '@danielmoncada/angular-datetime-picker';
 
+export const MY_NATIVE_FORMATS = {
+  fullPickerInput: { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' },
+  datePickerInput: { year: 'numeric', month: 'numeric', day: 'numeric' },
+  timePickerInput: { hour: 'numeric', minute: 'numeric' },
+  monthYearLabel: { year: 'numeric', month: 'short' },
+  dateAllyLabel: { day: 'numeric', year: 'numeric', month: 'long',  },
+  monthYearAllyLabel: { year: 'numeric', month: 'long' }
+
+}
 
 
 @NgModule({
@@ -32,12 +44,22 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    MatIconModule,
+    MatGridListModule
 
   ],
-  entryComponents:[DateComponent],
-  providers: [],
+  entryComponents: [DateComponent],
+  providers: [
+    { provide: OWL_DATE_TIME_FORMATS, useValue: MY_NATIVE_FORMATS }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
- }
+
+
+}
+
+
--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -1,42 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder } from '@angular/forms';
-import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-
-import * as _moment from 'moment';
-
-import { default as _rollupMoment } from 'moment';
-
-const moment = _rollupMoment || _moment;
-
-export const My_FORMATS = {
-  parse: {
-    dateInput: 'LL',
-  },
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'YYYY',
-    dateAllyLabel: 'LL',
-    monthYearAllyLabel: 'YYYY',
-  },
-};
 
 
 @Component({
   selector: 'app-date',
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.css'],
-  providers: [
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
-    },
-
-    {
-      provide: MAT_DATE_FORMATS, useValue: My_FORMATS
-    }
-  ],
+  providers: [],
 })
 
 
@@ -46,8 +16,7 @@ export class DateComponent implements OnInit {
 
 
   data = this.fb.group({
-    startdate: [''],
-    enddate: ['']
+    startdate: ['']
   });
 
   ngOnInit(): void {
