51fa02b44477b0b4fc1d72c201135a1ca40ecb01
Satyakrishna Jasti <satyajasti9@gmail.com>
2021-05-21 17:57:40 +0530

    added date format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,20 +12,21 @@ import { MatInputModule } from '@angular/material/input';
 import { DateComponent } from './date/date.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS } from '@danielmoncada/angular-datetime-picker';
 
-export const MY_NATIVE_FORMATS = {
-  fullPickerInput: { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' },
-  datePickerInput: { year: 'numeric', month: 'numeric', day: 'numeric' },
-  timePickerInput: { hour: 'numeric', minute: 'numeric' },
-  monthYearLabel: { year: 'numeric', month: 'short' },
-  dateAllyLabel: { day: 'numeric', year: 'numeric', month: 'long',  },
-  monthYearAllyLabel: { year: 'numeric', month: 'long' }
+import { OwlMomentDateTimeModule } from '@danielmoncada/angular-datetime-picker';
 
+export const MY_NATIVE_FORMATS = {
+  parseInt: 'DD-MM-YYYY',
+  fullPickerInput: 'l LT',
+  datePickerInput: 'DD MMMM YYYY',
+  timePickerInput: 'LT',
+  monthYearLabel: 'MMM YYYY',
+  dateAllyLabel: 'LL',
+  monthYearAllyLabel: 'MMMM YYYY'
 }
 
 
@@ -44,11 +45,11 @@ export const MY_NATIVE_FORMATS = {
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatNativeDateModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
     MatIconModule,
-    MatGridListModule
+    MatGridListModule,
+    OwlMomentDateTimeModule
 
   ],
   entryComponents: [DateComponent],
